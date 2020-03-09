1a54f4fb4b03d50bb91149f8d7f81a5bc5c5296c
Anirudh Kachroo <anirudh.kac@yahoo.com>
2020-03-09 21:16:41 +0530

    finish hello

--- Hello Flask/hello.py
+++ Hello Flask/hello.py
@@ -8,5 +8,27 @@ app = Flask(__name__)
 def home():
     return "Hello there, welcome to the flask app"
 
+
+
+# HTML can be sent using return
+@app.route("/about")
+def about():
+    return "<h1> This is a demo Flask app </h1>"
+
+
+@app.route("/users/<name>")
+def user(name):
+    return f"<h1> Hello {name}"
+
+
+admin_access = False
+
+# to redirect , use the redirect function , passing url_for with function name in quotes
+@app.route("/admin")
+def admin():
+    if(admin_access):
+        return "Hello Admin"
+    else:
+        return redirect(url_for("home"))
 if __name__ == "__main__":
     app.run()
